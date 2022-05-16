b68b14d65099e1b6fe7d3b5c3f02d3bd26fed8a7
Oscar MA <c.omellizo@sic.gov.co>
2022-05-15 19:11:08 -0500

    Primer programa, solicitud de datos, conversion y resultado

---
+++ basic/conversor.py
@@ -0,0 +1,7 @@
+pesos = input("¿Cantidad de pesos?")
+pesos = float(pesos)
+valor_dolar = 3875
+dolares = pesos / valor_dolar
+dolares = round(dolares, 2)
+dolares = str(dolares)
+print("Tienes " + dolares + " dolares") 
