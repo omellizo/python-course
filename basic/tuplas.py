97b60c18b1089637a6b0b0b67de241f0a8ae79a0
Oscar MA <c.omellizo@sic.gov.co>
2022-05-15 21:10:37 -0500

    Ejemplo de tupla

---
+++ basic/tuplas.py
@@ -0,0 +1,11 @@
+numeros = [1, 2, 3, 4, 5]
+numeros2 = [6, 7, 8, 9, 10]
+
+lista_final = numeros + numeros2
+print(lista_final)
+
+print(numeros * 3)
+
+mi_tupla = (1, 2, 3, 4, 5)
+for num in mi_tupla:
+    print(num)
