66a0ebb2b4c42f468c870ef0b1328b80a3e3bc52
Oscar MA <c.omellizo@sic.gov.co>
2022-05-15 19:21:23 -0500

    Condicionales

---
+++ basic/condicionales2.py
@@ -0,0 +1,11 @@
+from unicodedata import numeric
+
+
+numero = int(input("Escribe un numero"))
+
+if numero > 5:
+    print("Es mayor a 5")
+elif numero == 5:
+    print("Es igual a 5")
+else:
+    print("Es menor a 5")    
